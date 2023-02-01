f9527d812c1b5375ec829ba72d62c3bcc17ac3e7
santhoshkumarvvskrsk <santoshkumarv1991@gmail.com>
2023-02-01 11:31:36 +0530

    in check feature, unchecked the checked box

--- cypress/pages/CheckFunctionalityPage.js
+++ cypress/pages/CheckFunctionalityPage.js
@@ -10,7 +10,30 @@ class CheckFunctionalityPage{
         this.elements.checkboxLink().click();
     }
     clickCheckBox(){
-        cy.get('input').check().should('have.attr','checked');
+        cy.get('form[id="checkboxes"]').each(($row)=>{
+        //    if($row.attr('checked')){
+        //     cy.get('input').uncheck();
+        //    }
+               
+            // if(cy.get('input').not('.checked')){
+            //     cy.get('input').check();
+            // }else{
+                
+            // }
+            // if(cy.get('input').should('be.checked')){
+            //     cy.get('input').uncheck();
+            // }else{
+            //     cy.get('input').check();
+            // }
+            cy.get('input[type="checkbox"]').as('checkbox').invoke('is',':checked').then(checked=>{
+                if(checked){
+                    cy.get('@checkbox').uncheck();
+                }else{
+                    cy.get('@checkbox').check();
+                }
+            })
+        })
+        // cy.get('input').check().should('have.attr','checked');
     }
 }
 
